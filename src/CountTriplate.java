95892f03d4d7c55e7f56522737bd5ee2406c9320
nishi <17sepshaifali@gmail.com>
2021-10-17 18:30:42 +0530

    Geeks for Geeks

--- src/CountTriplate.java
+++ src/CountTriplate.java
@@ -2,13 +2,19 @@ import java.util.Arrays;
 
 public class CountTriplate {
 
-    int countTriplet(int[] arr, int n) {
+    public static void main(String[] args){
+        int[] arr= {12 ,8, 2, 11, 5, 14, 10};
+        int n= 7;
+        System.out.println(countTriplet(arr, n));
+    }
+
+    static int countTriplet(int[] arr, int n) {
         // code here
 
         int count = 0;
         Arrays.sort(arr);
         int start = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n-1; i++) {
             if (arr[start] + arr[i] == arr[i + 1]) {
                 count++;
                 start++;
